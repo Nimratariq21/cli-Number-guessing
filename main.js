466c454fa578f7b86e7510d25c0ce4c4894b6206
Nimra Tariq <160537529+Nimratariq21@users.noreply.github.com>
2024-03-24 01:54:39 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+console.log("randomNumber");
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedName",
+        type: "number",
+        message: "Please guess a number between 1-10: ",
+    }
+]);
+if (answer.userGuessedName === randomNumber) {
+    console.log("Congratulation! You guess a right number:");
+}
+else {
+    console.log("You guessed a wrong number:");
+}
